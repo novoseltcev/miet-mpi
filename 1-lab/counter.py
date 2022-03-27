5b1092b882bff7f9e51f7bb33378bb33785d25a2
st-a-novoseltcev <st.a.novoseltcev@gmail.com>
2022-03-27 02:49:19 +0000

    1-lab: Done

--- 1-lab/counter.py
+++ 1-lab/counter.py
@@ -11,10 +11,10 @@ if rank == 0:
         counter += 1
         if value == -1:
             print(f'Master handled {counter} messages '
-                  f'and terminated from slave-{source_rank} '
-                  f'with exec time: {MPI.Wtime() - begin_time}')
+                  f'and terminated from Slave#{source_rank} '
+                  f'(exec : {MPI.Wtime() - begin_time})')
             exit(0)
 else:
     for value in range(1000, -2, -1):
         comm.send((value, rank), dest=0)
-    print(f'Slave-{rank} stopped with exec time: {MPI.Wtime() - begin_time}')
+    print(f'Slave#{rank} stopped (exec: {MPI.Wtime() - begin_time})')
